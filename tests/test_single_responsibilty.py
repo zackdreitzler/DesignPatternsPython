a2a133cc272a69ebac2cb625a498d2601c837ab5
zackdreitzler <zdreitzler@gmail.com>
2023-12-02 18:57:14 -0500

    Created tests for the SingleResponsibilty files

---
+++ tests/test_single_responsibilty.py
@@ -0,0 +1,72 @@
+"""This module tests the functionality of the SOLID files"""
+import pytest
+from SOLID.single_responsibility_after import Order, PaymentProcessor
+
+
+@pytest.fixture
+def valid_order() -> Order:
+    items: list[str] = ["Keyboard", "Monitor"]
+    quantites: list[int] = [1, 2]
+    prices: list[int] = [50, 65]
+
+    return Order(items, quantites, prices)
+
+
+@pytest.fixture
+def empty_order() -> Order:
+    return Order()
+
+
+@pytest.fixture
+def payment_processor() -> PaymentProcessor:
+    return PaymentProcessor()
+
+
+class TestOrder:
+    """Test the functionality of the Order class"""
+
+    def test_adding_item_to_nonempty_order(self, valid_order):
+        """Test adding an item to a nonempty order"""
+        my_order: Order = valid_order
+        my_order.add_item("Mouse", 1, 25)
+
+        assert "Mouse" in my_order.items
+        assert 1 in my_order.quantites
+        assert len(my_order.quantites) == 3
+        assert 25 in my_order.prices
+        assert len(my_order.prices) == 3
+
+    def test_adding_item_to_empty_order(self, empty_order):
+        """Test adding an item to an empty order"""
+        my_order: Order = empty_order
+        my_order.add_item("Mouse", 1, 25)
+
+        assert "Mouse" in my_order.items
+        assert 1 in my_order.quantites
+        assert len(my_order.quantites) == 1
+        assert 25 in my_order.prices
+        assert len(my_order.prices) == 1
+
+    def test_getting_total_price_with_nonempty_order(self, valid_order):
+        """Test getting the total price of a nonempty order"""
+        assert valid_order.total_price() == 180
+
+    def test_getting_total_price_with_empty_order(self, empty_order):
+        """Test getting the total price of an empty order"""
+        assert empty_order.total_price() == 0
+
+
+class TestPaymentProcessor:
+    """Test the functionality of the PaymentProcessor class"""
+
+    def test_pay_with_debit(self, valid_order, payment_processor):
+        """Test paying an order with a debit card"""
+        payment_processor.pay_debit(valid_order, "123456")
+
+        assert valid_order.status == "paid"
+
+    def test_pay_with_credit(self, valid_order, payment_processor):
+        """Test paying an order with a credit card"""
+        payment_processor.pay_credit(valid_order, "123456")
+
+        assert valid_order.status == "paid"
