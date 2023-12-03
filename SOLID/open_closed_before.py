58652275b22224b6dcfe1223ffa23d8d47a245c6
zackdreitzler <zdreitzler@gmail.com>
2023-12-02 21:24:00 -0500

    Clearing out order from this class

--- SOLID/open_closed_before.py
+++ SOLID/open_closed_before.py
@@ -14,27 +14,7 @@ Here we see that the PaymentProcessor class violates the open closed principle.
 We can see this because to add another payment type, the class would need changes.
 
 """
-from dataclasses import dataclass, field
-
-
-@dataclass
-class Order:
-    """An Order within the system"""
-
-    items: list[str] = field(default_factory=list)
-    quantites: list[int] = field(default_factory=list)
-    prices: list[int] = field(default_factory=list)
-    status: str = field(default="open", init=False)
-
-    def add_item(self, name: str, quantity: int, price: int) -> None:
-        """Adds an item to the order"""
-        self.items.append(name)
-        self.quantites.append(quantity)
-        self.prices.append(price)
-
-    def total_price(self) -> int:
-        """Calculates and returns the total price of the order"""
-        return sum([quantity * price for quantity, price in zip(self.quantites, self.prices)])
+from SOLID.order import Order
 
 
 class PaymentProcessor:
